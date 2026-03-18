72858ed233f01fee70e381e5ace685d964146717
Jitesh596 <jtskumar05@gmail.com>
2026-03-18 19:42:11 +0530

    make books.js file

---
+++ Library-System/js/books.js
@@ -0,0 +1,26 @@
+const books = [
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    category: "Computer Science",
+    status: "Available"
+  },
+  {
+    title: "Operating Systems",
+    author: "Abraham Silberschatz",
+    category: "Engineering",
+    status: "Issued"
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    author: "J.K. Rowling",
+    category: "Fiction",
+    status: "Available"
+  },
+  {
+    title: "Effective Python",
+    author: "Brett Slatkin",
+    category: "Programming",
+    status: "Available"
+  }
+];
