0d4238fa7ece6b506a1158cbc318ce17cc6a0837
Himanshu Saini <himanshu.saini.cse@gmail.com>
2026-04-06 14:05:00 +0530

    animation removed

--- Library-System/js/animation2.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-  document.querySelectorAll(".reveal").forEach(el => {
-    const top = el.getBoundingClientRect().top;
-    if (top < window.innerHeight - 100) {
-      el.classList.add("active");
-    }
-  });
-});
--- Library-System/js/anmation7.js
+++
@@ -1,6 +0,0 @@
-const glow = document.querySelector(".mouse-glow");
-
-document.addEventListener("mousemove", (e) => {
-  glow.style.left = e.clientX + "px";
-  glow.style.top = e.clientY + "px";
-});
